a88a3b284251518647a5207c275c1d7c3946e1a6
032-Abhishek Choksi <19bmiit032@gmail.com>
2022-06-08 21:25:25 +0530

    Why use ExpressJS | RESTful API using NodeJS and MongoDB

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log('I am running... and i am changed');
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        if (req.method === 'GET') {
+            console.log("It's a GET Method");
+        }
+        res.write("Your on Home Page");
+        res.end();
+    } else if (req.url === '/another') {
+        res.write("Your on Another Page");
+        res.end();
+    }
+})
+
+
+server.listen(3000, () => {
+    console.log('Server started on port 3000...');
+})
